f9a31caf08c71d27c05846ca50642fdc696af105
Damilola Alao <oluwadamilola.alao@gmail.com>
2022-11-17 05:27:14 +0100

    Update aggregate action name from pipeline to aggregate

--- src/index.ts
+++ src/index.ts
@@ -288,7 +288,7 @@ export class MongoDBDataAPI<InnerDoc = Document> {
   public aggregate<T extends Array<any>>(
     params: ExtendBaseParams<{ pipeline: Array<Document> }>
   ) {
-    return this.$$action<{ documents: T }>('pipeline', params)
+    return this.$$action<{ documents: T }>('aggregate', params)
   }
 }
 
